2d30d4aa9838f5d982b0424806a9bd93b982b25f
Victor Huang <i@imvictor.tech>
2018-05-06 11:45:52 +0800

    improved independence

--- install.sh
+++ install.sh
@@ -6,10 +6,13 @@ cp -a files/var/* /var/
 cp -a files/userdisk/* /userdisk/
 printf "Your network ID? Input: "
 read netid
+echo > /userdisk/ZeroTier/run.sh
+echo "export LD_LIBRARY_PATH=/userdisk/ZeroTier/usr/lib:\$LD_LIBRARY_PATH" >> /userdisk/ZeroTier/run.sh
+echo "/userdisk/ZeroTier/usr/bin/zerotier-one -d" >> /userdisk/ZeroTier/run.sh
+echo "/userdisk/ZeroTier/usr/bin/zerotier-cli -join $netid" >> /userdisk/ZeroTier/run.sh
+chmod a+x /userdisk/ZeroTier/run.sh
 echo > /tmp/tmp_zerotier.sh
-echo "export LD_LIBRARY_PATH=/userdisk/ZeroTier/usr/lib:\$LD_LIBRARY_PATH" >> /tmp/tmp_zerotier.sh
-echo "/userdisk/ZeroTier/usr/bin/zerotier-one -d" >> /tmp/tmp_zerotier.sh
-echo "/userdisk/ZeroTier/usr/bin/zerotier-cli -join $netid" >> /tmp/tmp_zerotier.sh
+echo "sh /userdisk/ZeroTier/run.sh" >> /tmp/tmp_zerotier.sh
 echo >> /tmp/tmp_zerotier.sh
 mv /etc/rc.local /etc/rc.local.zerotier_backup
 cat /tmp/tmp_zerotier.sh /etc/rc.local.zerotier_backup > /etc/rc.local
